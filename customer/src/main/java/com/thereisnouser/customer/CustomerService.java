3e9fc1b7b047c5869eb887363aec59daa41488bf
thereisnouser <jane.doe.mail@yandex.com>
2023-11-25 13:18:57 +0300

    Configure communication between customer and fraud services by rest template

--- customer/src/main/java/com/thereisnouser/customer/CustomerService.java
+++ customer/src/main/java/com/thereisnouser/customer/CustomerService.java
@@ -1,9 +1,15 @@
 package com.thereisnouser.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.text.MessageFormat;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(
+        CustomerRepository customerRepository,
+        RestTemplate restTemplate
+) {
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -12,6 +18,16 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .email(request.email())
                 .build();
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException(MessageFormat.format("customer {0} is fraudster", customer.getId()));
+        }
     }
 }
---
+++ customer/src/main/java/com/thereisnouser/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.thereisnouser.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
