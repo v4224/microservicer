de9db0c2e6fb74289271e8388a55314d291441e3
thereisnouser <jane.doe.mail@yandex.com>
2023-11-29 20:22:41 +0300

    Add micrometer tracing and zipkin

--- customer/src/main/java/com/thereisnouser/customer/CustomerConfig.java
+++ customer/src/main/java/com/thereisnouser/customer/CustomerConfig.java
@@ -13,4 +13,14 @@ public class CustomerConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    // https://stackoverflow.com/a/65724088
+    /*
+     * // https://www.appsdeveloperblog.com/micrometer-and-zipkin-in-spring-boot/
+     * //     -> Tracing HTTP Request Sent with Feign
+     * @Bean
+     * public Capability capability(final MeterRegistry meterRegistry) {
+     *     return new MicrometerCapability(meterRegistry);
+     * }
+     */
 }
